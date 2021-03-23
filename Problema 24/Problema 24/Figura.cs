cbefc2b4ff3f718dd05e37429d046a4ddc8b20f7
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-03-23 17:38:32 -0600

    Add files via upload

---
+++ Problema 24/Problema 24/Figura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema_24
+{
+    class Figura
+    {
+        public void imprimirfigura(ListBox lista) 
+        {
+            string cadena = "";
+            for(int i=1; i<5; i++) 
+            {
+                for(int j = 1; j <= i; j++) 
+                {
+                    cadena = cadena + "*";
+                }
+                lista.Items.Add(cadena);
+                cadena = "";
+
+            }
+   
+        }
+
+    }
+}
